2ff9a859092c6519eb0f445ea3041d1ba2e92991
simopil <46675925+simopil@users.noreply.github.com>
2019-01-22 00:54:02 +0100

    Indentation, remove confirm, readability

--- uninstall.sh
+++ uninstall.sh
@@ -1,41 +1,45 @@
 #!/bin/bash
 
+function uninstall
+{
+    echo -e "\nPreparing to uninstall ..."
+
+    #Service
+    echo -e "\nDisabling service ..."
+    systemctl disable prime_switch
+    echo "Done."
+  
+    #File install
+    echo -e "\nRemoving files ..."
+    rm -r                           /etc/prime
+    rm                              /etc/systemd/system/prime_switch.service
+    rm                              /etc/systemd/system/prime_logout.waiting.service
+    rm                              /usr/bin/vga
+    rm                              /etc/modprobe.d/suse-prime.conf
+    echo "Done."
+
+    #Modprobe_rules
+    echo -e "\nRestoring modprobe rules ..."
+    mkinitrd
+    echo "Done."
+    echo -e "\nScript uninstalled successfully!\n"
+}
+
 if [[ $EUID > 0 ]]
-    then echo "Please run as root"
+    then echo -e "\nPlease run as root\n"
     exit
 fi
 
 if [ ! -d /etc/prime/services ] ; then
-  echo "This script is NOT installed, aborting ..."
-  exit
+    echo -e "\nThis script is NOT installed, aborting ...\n"
+    exit
 fi
 
-echo "Preparing to uninstall ..."
+echo
+read -p "Are you sure that you want to remove Nvidia Optimus Switch from the system? (y|n): " confirm
 
-#Service
-echo "Disabling service ..."
-systemctl disable prime_switch
-echo "Done."
-
-#File install
-echo "Removing files ..."
-rm -r                           /etc/prime/services
-rm                              /etc/prime/config
-rm                              /etc/systemd/system/prime_switch.service
-rm                              /etc/systemd/system/prime_logout.waiting.service
-rm                              /usr/bin/vga
-rm                              /etc/modprobe.d/suse-prime.conf
-echo "Done."
-
-#Modprobe_rules
-echo "Restoring modprobe rules ..."
-mkinitrd
-echo "Done."
-
-echo "Script uninstalled successfully!"
-
-
-
-
-
-
+case "$confirm" in
+    y|Y ) uninstall;;
+    n|N ) echo -e "\nAborted\n";;
+      * ) echo -e "\nInvalid answer, aborted\n";;
+esac
